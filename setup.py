f6b4107dda482ec8c9e901f4bb80c89fc220bfcc
Joseph Reisinger <joeraii@gmail.com>
2012-05-02 15:57:53 -0700

    update setup

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
-from distutils.core import setup
-setup(name='vowpal_porpoise',
+import setuptools
+import setuptools.extension
+
+setuptools.setup(name='vowpal_porpoise',
       description='Lightweight vowpal wabbit wrapper',
       version='0.1',
       author='Austin Waters',
       author_email='austin.waters@gmail.com',
-      packages=['vowpal_porpoise'],
+      packages=setuptools.find_packages(),
+      install_requires=[],
       )
