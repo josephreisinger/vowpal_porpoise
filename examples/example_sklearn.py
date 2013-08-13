86ff05e1236638a2c9c1bcd3d0aa83608e3d12c0
Daniel Duckworth <dux@premise.com>
2013-08-12 20:26:36 -0700

    Conform to PEP8.

--- examples/example_sklearn.py
+++ examples/example_sklearn.py
@@ -9,56 +9,56 @@ from vowpal_porpoise.sklearn import VW_Classifier
 
 class Array2Dict(TransformerMixin):
 
-  def fit(self, X, y=None):
-    return self
+    def fit(self, X, y=None):
+        return self
 
-  def transform(self, X):
-    n_samples, n_features = np.shape(X)
-    result = []
-    for i in range(n_samples):
-      result.append({
-        str(j): X[i,j]
-        for j in range(n_features)
-        if X[i,j] != 0
-      })
-    return result
+    def transform(self, X):
+        n_samples, n_features = np.shape(X)
+        result = []
+        for i in range(n_samples):
+            result.append({
+                          str(j): X[i, j]
+                          for j in range(n_features)
+                          if X[i, j] != 0
+                          })
+        return result
 
 
 def main():
-  # parameters to cross-validate over
-  parameters = {
-    'l2': np.logspace(-5, 0, num=6),
-  }
+    # parameters to cross-validate over
+    parameters = {
+        'l2': np.logspace(-5, 0, num=6),
+    }
 
-  # load iris data in, make a binary decision problem out of it
-  data = load_digits()
+    # load iris data in, make a binary decision problem out of it
+    data = load_digits()
 
-  X = Array2Dict().fit_transform(data.data)
-  y = 2 * (data.target >= 5) - 1
+    X = Array2Dict().fit_transform(data.data)
+    y = 2 * (data.target >= 5) - 1
 
-  i = int(0.8 * len(X))
-  X_train, X_test = X[:i], X[i:]
-  y_train, y_test = y[:i], y[i:]
+    i = int(0.8 * len(X))
+    X_train, X_test = X[:i], X[i:]
+    y_train, y_test = y[:i], y[i:]
 
-  # do the actual learning
-  gs = GridSearchCV(
-    VW_Classifier(loss='logistic', moniker='example_sklearn', passes=10, silent=True, learning_rate=10),
-    param_grid=parameters,
-    score_func=f1_score,
-    cv=StratifiedKFold(y_train),
-  ).fit(X_train, y_train)
+    # do the actual learning
+    gs = GridSearchCV(
+        VW_Classifier(loss='logistic', moniker='example_sklearn',
+                      passes=10, silent=True, learning_rate=10),
+        param_grid=parameters,
+        score_func=f1_score,
+        cv=StratifiedKFold(y_train),
+    ).fit(X_train, y_train)
 
-  # print out results from cross-validation
-  estimator = gs.best_estimator_
-  score     = gs.best_score_
-  print 'Achieved a F1 score of %f using l2 == %f during cross validation' % (score, estimator.l2)
+    # print out results from cross-validation
+    estimator = gs.best_estimator_
+    score = gs.best_score_
+    print 'Achieved a F1 score of %f using l2 == %f during cross validation' % (score, estimator.l2)
 
-  # print confusion matrix on test data
-  import ipdb; ipdb.set_trace()
-  y_est = estimator.fit(X_train, y_train).predict(X_test)
-  print 'Confusing Matrix:'
-  print confusion_matrix(y_test, y_est)
+    # print confusion matrix on test data
+    y_est = estimator.fit(X_train, y_train).predict(X_test)
+    print 'Confusing Matrix:'
+    print confusion_matrix(y_test, y_est)
 
 
 if __name__ == '__main__':
-  main()
+    main()
--- vowpal_porpoise/sklearn.py
+++ vowpal_porpoise/sklearn.py
@@ -8,174 +8,174 @@ import vowpal_porpoise
 
 
 class _VW(sklearn.base.BaseEstimator):
-  """scikit-learn interface for Vowpal Wabbit
 
-  Only works for regression and binary classification.
-  """
+    """scikit-learn interface for Vowpal Wabbit
 
-  def __init__(self,
-        logger              = None,
-        vw                  = 'vw',
-        moniker             = 'moniker',
-        name                = None,
-        bits                = None,
-        loss                = None,
-        passes              = 10,
-        log_stderr_to_file  = False,
-        silent              = False,
-        l1                  = None,
-        l2                  = None,
-        learning_rate       = None,
-        quadratic           = None,
-        audit               = None,
-        power_t             = None,
-        adaptive            = False,
-        working_dir         = None,
-        decay_learning_rate = None,
-        initial_t           = None,
-        minibatch           = None,
-        total               = None,
-        node                = None,
-        unique_id           = None,
-        span_server         = None,
-        bfgs                = None,
-        oaa                 = None,
-        old_model           = None,
-        incremental         = False,
-        mem                 = None
-      ):
-    self.logger              = logger
-    self.vw                  = vw
-    self.moniker             = moniker
-    self.name                = name
-    self.bits                = bits
-    self.loss                = loss
-    self.passes              = passes
-    self.log_stderr_to_file  = log_stderr_to_file
-    self.silent              = silent
-    self.l1                  = l1
-    self.l2                  = l2
-    self.learning_rate       = learning_rate
-    self.quadratic           = quadratic
-    self.audit               = audit
-    self.power_t             = power_t
-    self.adaptive            = adaptive
-    self.working_dir         = working_dir
-    self.decay_learning_rate = decay_learning_rate
-    self.initial_t           = initial_t
-    self.minibatch           = minibatch
-    self.total               = total
-    self.node                = node
-    self.unique_id           = unique_id
-    self.span_server         = span_server
-    self.bfgs                = bfgs
-    self.oaa                 = oaa
-    self.old_model           = old_model
-    self.incremental         = incremental
-    self.mem                 = mem
-
-
-  def fit(self, X, y):
-    """Fit Vowpal Wabbit
-
-    Parameters
-    ----------
-    X: [{<feature name>: <feature value>}]
-        input features
-    y: [int or float]
-        output labels
+    Only works for regression and binary classification.
     """
-    examples = _as_vw_strings(X, y)
 
-    # initialize model
-    self.vw_ = vowpal_porpoise.VW(
-        logger              = self.logger,
-        vw                  = self.vw,
-        moniker             = self.moniker,
-        name                = self.name,
-        bits                = self.bits,
-        loss                = self.loss,
-        passes              = self.passes,
-        log_stderr_to_file  = self.log_stderr_to_file,
-        silent              = self.silent,
-        l1                  = self.l1,
-        l2                  = self.l2,
-        learning_rate       = self.learning_rate,
-        quadratic           = self.quadratic,
-        audit               = self.audit,
-        power_t             = self.power_t,
-        adaptive            = self.adaptive,
-        working_dir         = self.working_dir,
-        decay_learning_rate = self.decay_learning_rate,
-        initial_t           = self.initial_t,
-        minibatch           = self.minibatch,
-        total               = self.total,
-        node                = self.node,
-        unique_id           = self.unique_id,
-        span_server         = self.span_server,
-        bfgs                = self.bfgs,
-        oaa                 = self.oaa,
-        old_model           = self.old_model,
-        incremental         = self.incremental,
-        mem                 = self.mem,
-    )
+    def __init__(self,
+                 logger=None,
+                 vw='vw',
+                 moniker='moniker',
+                 name=None,
+                 bits=None,
+                 loss=None,
+                 passes=10,
+                 log_stderr_to_file=False,
+                 silent=False,
+                 l1=None,
+                 l2=None,
+                 learning_rate=None,
+                 quadratic=None,
+                 audit=None,
+                 power_t=None,
+                 adaptive=False,
+                 working_dir=None,
+                 decay_learning_rate=None,
+                 initial_t=None,
+                 minibatch=None,
+                 total=None,
+                 node=None,
+                 unique_id=None,
+                 span_server=None,
+                 bfgs=None,
+                 oaa=None,
+                 old_model=None,
+                 incremental=False,
+                 mem=None
+                 ):
+        self.logger = logger
+        self.vw = vw
+        self.moniker = moniker
+        self.name = name
+        self.bits = bits
+        self.loss = loss
+        self.passes = passes
+        self.log_stderr_to_file = log_stderr_to_file
+        self.silent = silent
+        self.l1 = l1
+        self.l2 = l2
+        self.learning_rate = learning_rate
+        self.quadratic = quadratic
+        self.audit = audit
+        self.power_t = power_t
+        self.adaptive = adaptive
+        self.working_dir = working_dir
+        self.decay_learning_rate = decay_learning_rate
+        self.initial_t = initial_t
+        self.minibatch = minibatch
+        self.total = total
+        self.node = node
+        self.unique_id = unique_id
+        self.span_server = span_server
+        self.bfgs = bfgs
+        self.oaa = oaa
+        self.old_model = old_model
+        self.incremental = incremental
+        self.mem = mem
 
-    # add examples to model
-    with self.vw_.training():
-      for instance in examples:
-        self.vw_.push_instance(instance)
+    def fit(self, X, y):
+        """Fit Vowpal Wabbit
 
-    # learning done after "with" statement
-    return self
+        Parameters
+        ----------
+        X: [{<feature name>: <feature value>}]
+            input features
+        y: [int or float]
+            output labels
+        """
+        examples = _as_vw_strings(X, y)
 
-  def predict(self, X):
-    """Fit Vowpal Wabbit
+        # initialize model
+        self.vw_ = vowpal_porpoise.VW(
+            logger=self.logger,
+            vw=self.vw,
+            moniker=self.moniker,
+            name=self.name,
+            bits=self.bits,
+            loss=self.loss,
+            passes=self.passes,
+            log_stderr_to_file=self.log_stderr_to_file,
+            silent=self.silent,
+            l1=self.l1,
+            l2=self.l2,
+            learning_rate=self.learning_rate,
+            quadratic=self.quadratic,
+            audit=self.audit,
+            power_t=self.power_t,
+            adaptive=self.adaptive,
+            working_dir=self.working_dir,
+            decay_learning_rate=self.decay_learning_rate,
+            initial_t=self.initial_t,
+            minibatch=self.minibatch,
+            total=self.total,
+            node=self.node,
+            unique_id=self.unique_id,
+            span_server=self.span_server,
+            bfgs=self.bfgs,
+            oaa=self.oaa,
+            old_model=self.old_model,
+            incremental=self.incremental,
+            mem=self.mem,
+        )
 
-    Parameters
-    ----------
-    X: [{<feature name>: <feature value>}]
-        input features
-    """
-    examples = _as_vw_strings(X)
+        # add examples to model
+        with self.vw_.training():
+            for instance in examples:
+                self.vw_.push_instance(instance)
 
-    # add test examples to model
-    with self.vw_.predicting():
-      for instance in examples:
-        self.vw_.push_instance(instance)
+        # learning done after "with" statement
+        return self
 
-    # read out predictions
-    predictions = np.asarray(list(self.vw_.read_predictions_()))
+    def predict(self, X):
+        """Fit Vowpal Wabbit
 
-    return predictions
+        Parameters
+        ----------
+        X: [{<feature name>: <feature value>}]
+            input features
+        """
+        examples = _as_vw_strings(X)
+
+        # add test examples to model
+        with self.vw_.predicting():
+            for instance in examples:
+                self.vw_.push_instance(instance)
+
+        # read out predictions
+        predictions = np.asarray(list(self.vw_.read_predictions_()))
+
+        return predictions
 
 
 class VW_Regressor(sklearn.base.RegressorMixin, _VW):
-  pass
+    pass
 
 
 class VW_Classifier(sklearn.base.ClassifierMixin, _VW):
 
-  def predict(self, X):
-    result = super(VW_Classifier, self).predict(X)
-    result = 2 * (result >= 0) - 1
-    return result
+    def predict(self, X):
+        result = super(VW_Classifier, self).predict(X)
+        result = 2 * (result >= 0) - 1
+        return result
 
 
 def _as_vw_string(x, y=None):
-  """Convert {feature: value} to something _VW understands
+    """Convert {feature: value} to something _VW understands
 
-  Parameters
-  ----------
-  x : {<feature>: <value>}
-  y : int or float
-  """
-  result = str(y)
-  x = " ".join(["%s:%f" % (key, value) for (key, value) in x.items()])
-  return result + " | " + x
+    Parameters
+    ----------
+    x : {<feature>: <value>}
+    y : int or float
+    """
+    result = str(y)
+    x = " ".join(["%s:%f" % (key, value) for (key, value) in x.items()])
+    return result + " | " + x
 
 
 def _as_vw_strings(X, y=None):
-  n_samples = np.shape(X)[0]
-  if y is None:
-    y = np.ones(n_samples)
-  return [_as_vw_string(X[i], y[i]) for i in range(n_samples)]
+    n_samples = np.shape(X)[0]
+    if y is None:
+        y = np.ones(n_samples)
+    return [_as_vw_string(X[i], y[i]) for i in range(n_samples)]
